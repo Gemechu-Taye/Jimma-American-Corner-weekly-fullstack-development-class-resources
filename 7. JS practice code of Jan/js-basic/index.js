df83f4976d331480dc52b4f4101e6965335d6312
Gemechu Taye <gemetayeh@gmail.com>
2022-03-05 16:03:11 +0530

    Javascript practice source code

---
+++ 7. JS practice code of Jan/js-basic/index.js
@@ -0,0 +1,21 @@
+/* var, let, const
+*/
+
+/* math operation (using const)
+*/
+
+/* Data type
+*/
+
+/* template literals
+*/
+
+/* challange 1 BMI Calculation
+*/
+
+/* if else 
+Challange 2 BMI Calculation(Next Week)
+*/
+
+
+
---
+++ 7. JS practice code of Jan/js-class-jac/script.js
@@ -0,0 +1,26 @@
+/* var, let, const
+*/
+let age = 21;
+console.log("Age");
+
+let js = "amazing";
+console.log("js");
+
+/* math operation (using const)
+*/
+
+/* Data type
+*/
+
+/* template literals
+*/
+
+/* challange 1 BMI Calculation
+*/
+
+/* if else 
+Challange 2 BMI Calculation(Next Week)
+*/
+
+
+
